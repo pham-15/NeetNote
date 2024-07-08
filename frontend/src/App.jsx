20cfe4ec9896b5b100c2bc6bcd2ce37bf9f7873c
Pham15 <phamalex05@gmail.com>
2024-07-08 16:02:37 -0500

    added pages folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import CreateNote from './pages/CreateNote';
+import ShowNote from './pages/ShowNote';
+import EditNote from './pages/EditNote';
+import DeleteBook from './pages/DeleteNote'
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white'>App</div>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/notes/create' element={<CreateNote />}/>
+      <Route path='notes/details/:id' element={<ShowNote />}/>
+      <Route path='notes/edit/:id' element={<EditNote />}/>
+      <Route path='notes/delete/:id' element={<DeleteBook />}/>
+    </Routes>
   )
 }
 
