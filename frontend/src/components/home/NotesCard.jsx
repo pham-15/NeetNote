2d9d80c3e500423d1f00fdcc9c0154fd01b5c220
Pham15 <phamalex05@gmail.com>
2024-07-31 11:27:21 -0500

    added a modal feature

--- frontend/src/components/home/NotesCard.jsx
+++ frontend/src/components/home/NotesCard.jsx
@@ -4,39 +4,13 @@ import { BiUserCircle } from "react-icons/bi";
 import { AiOutlineEdit } from "react-icons/ai";
 import { BsInfoCircle } from "react-icons/bs";
 import { MdOutlineDelete } from "react-icons/md";
+import NoteSingleCard from "./NoteSingleCard";
 
 const NotesCard = ({ notes }) => {
   return (
     <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {notes.map((item) => (
-        <div
-          key={item._id}
-          className="border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl"
-        >
-          <h2 className="absolute top-1 right-2 px-4 py-1 bg-red-300 rounded-lg">
-            {item.problemNumber}
-          </h2>
-          <h4 className="my-2 text-gray-500">{item.title}</h4>
-          <div className="flex justify-start items-center gap-x-2">
-            <PiBookOpenTextLight className="text-red-300 text-2xl" />
-            <h2 className="my-1">{item.title}</h2>
-          </div>
-          <div className="flex justify-start items-center gap-x-2">
-            <BiUserCircle className="text-red-300 text-2xl" />
-            <h2 className="my-1">{item.difficulty}</h2>
-          </div>
-          <div className="flex justify-between items-center gap-x-2 mt-4 p-4">
-            <Link to={`/notes/details/${item._id}`}>
-              <BsInfoCircle className="text-2xl text-green-800 hover:text-black" />
-            </Link>
-            <Link to={`/notes/edit/${item._id}`}>
-              <AiOutlineEdit className="text-2xl text-yellow-600 hover:text-black" />
-            </Link>
-            <Link to={`/notes/delete/${item._id}`}>
-              <MdOutlineDelete className="text-2xl text-red-600 hover:text-black" />
-            </Link>
-          </div>
-        </div>
+        <NoteSingleCard key={item._id} note={item} />
       ))}
     </div>
   );
