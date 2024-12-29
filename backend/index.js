df756951e16596e939449ad92748c42b5df976f6
Alex Pham <136045296+pham-15@users.noreply.github.com>
2024-12-28 21:29:11 -0600

    Set up middleware

--- backend/index.js
+++ backend/index.js
@@ -2,4 +2,29 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+
 dotenv.config();
+
+const app = express();
+
+// Middleware setup
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (request, response) => {
+  console.log(
+    "request: ",
+    request,
+    "request.method: ",
+    request.method,
+    "request.url: ",
+    request.url
+  );
+  response.status(200).send("Welcome to NeetNote");
+});
+
+app.listen(PORT, () => {
+  console.log(`App is listening to port: ${PORT}`);
+});
