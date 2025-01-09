2aa7c56f94bb278cc459e6dc2740f56ccc5a1605
Alex Pham <136045296+pham-15@users.noreply.github.com>
2025-01-09 00:55:34 -0600

    Add method to delete a user's note

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -75,3 +75,33 @@ export const createNewNote = async (req, res) => {
       .json({ message: "Error in createNewNote: ", error: error.message });
   }
 };
+
+export const deleteNote = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    const { problemNumber } = req.body;
+
+    // const problemNumber = req.query.problemNumber;
+
+    if (!user) {
+      return res.status(404).json({ message: "deleteNote: User not found" });
+    }
+
+    const noteExists = user.notes.some(
+      (note) => note.problemNumber === problemNumber
+    );
+    if (!noteExists) {
+      return res.status(404).json({ message: "Note not found" });
+    }
+
+    user.notes.pull({ problemNumber: problemNumber });
+    await user.save();
+
+    res.status(200).json({ message: "Note deleted successfully" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error in deleteNote: ", error: error.message });
+  }
+};
--- backend/routes/notesRoutes.js
+++ backend/routes/notesRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
 import { loginUser } from "../controllers/userController.js";
-import { getUserNotesById } from "../controllers/noteController.js";
-import { createNewNote } from "../controllers/noteController.js";
+import {
+  getUserNotesById,
+  createNewNote,
+  deleteNote,
+} from "../controllers/noteController.js";
 
 const router = express.Router();
 
@@ -11,5 +14,7 @@ router.post("/login", loginUser);
 router.get("/:id/allNotes", protect, getUserNotesById);
 
 router.post("/:id/createNote", protect, createNewNote);
+
+router.delete("/:id/deleteNote", protect, deleteNote);
 
 export default router;
